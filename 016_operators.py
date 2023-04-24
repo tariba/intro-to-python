c4e4862a3786b597c20d0f4d62c4d3149800a274
tariba <tariqxa@gmail.com>
2023-04-24 10:51:53 +0100

    floor division task completed

--- 016_operators.py
+++ 016_operators.py
@@ -60,7 +60,7 @@ print(f"2 - 3 = {subtracted} (should be -1)")
 # == Division ==
 
 divided = 2 / 3
-print(f"2 ? 3 = {divided} (should be 0.6666666666666666)")
+print(f"2 / 3 = {divided} (should be 0.6666666666666666)")
 
 # This kind of 'decimal point' number, 0.6666666666666666 is
 # called a float, by the way, meaning 'floating point'.
@@ -69,13 +69,13 @@ print(f"2 ? 3 = {divided} (should be 0.6666666666666666)")
 # Sometimes known as "remainder if we divide 3 by 2"
 
 modulus = 3 % 2
-print(f"3 ? 2 = {modulus} (should be 1)")
+print(f"3 % 2 = {modulus} (should be 1)")
 
 # == Floor division ==
 # Sometimes known as "division without remainder"
 
-# floor_divided = 2 ? 3
-# print(f"2 ? 3 = {floor_divided} (should be 0)")
+floor_divided = 2 // 3
+print(f"2 // 3 = {floor_divided} (should be 0)")
 
 # == Exponentiation ==
 # Sometimes known as "2 to the power of 3"

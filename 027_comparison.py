4ded335876facc26b263949b82b25c2ff7bbc55d
tariba <tariqxa@gmail.com>
2023-04-24 13:09:08 +0100

    comparision task completed

--- 027_comparison.py
+++ 027_comparison.py
@@ -15,7 +15,10 @@ print("")
 print("Function: a_is_equal_to_b")
 
 def a_is_equal_to_b(a, b):
-  return a == b
+  if a == b:
+    return True
+  else:
+    return False
 
 check_that_these_are_equal(
   a_is_equal_to_b(1, 1),
@@ -39,8 +42,11 @@ print("Function: a_is_less_than_b")
 
 def a_is_less_than_b(a, b):
   # Uncomment this next line and replace ?? with the right operator
-  # return a ?? b
-  pass
+  if a < b :
+    return True
+  else: 
+    return False
+  
 
 check_that_these_are_equal(
   a_is_less_than_b(1, 2),
@@ -63,8 +69,10 @@ print("")
 print("Function: a_is_greater_than_b")
 
 def a_is_greater_than_b(a, b):
-  # return a ?? b
-  pass
+  if a > b :
+    return True
+  else:
+    return False
 
 check_that_these_are_equal(
   a_is_greater_than_b(1, 2),
@@ -87,8 +95,11 @@ print("")
 print("Function: a_is_less_than_or_equal_to_b")
 
 def a_is_less_than_or_equal_to_b(a, b):
-  # return a ?? b
-  pass
+  if a <= b:
+    return True
+  else:
+    return False
+  
 
 check_that_these_are_equal(
   a_is_less_than_or_equal_to_b(1, 2),
@@ -111,8 +122,11 @@ print("")
 print("Function: a_is_greater_than_or_equal_to_b")
 
 def a_is_greater_than_or_equal_to_b(a, b):
-  # return a ?? b
-  pass
+  if a >= b:
+    return True
+  else: 
+    return False
+
 
 check_that_these_are_equal(
   a_is_greater_than_or_equal_to_b(1, 2),
@@ -135,8 +149,11 @@ print("")
 print("Function: a_is_not_equal_to_b")
 
 def a_is_not_equal_to_b(a, b):
-  # return a ?? b
-  pass
+  if a != b :
+    return True
+  else:
+    return False
+  
 
 check_that_these_are_equal(
   a_is_not_equal_to_b(1, 2),
@@ -161,8 +178,11 @@ print("Function: a_is_within_b")
 # May be a little tricky — search for
 # "python check if string contains substring"
 def a_is_within_b(a, b):
-  # return a ?? b
-  pass
+  if a in b:
+    return True
+  else:
+    return False
+  
 
 check_that_these_are_equal(
   a_is_within_b("e", "hello"),

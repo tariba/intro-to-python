7c39f5091562e34f5c57cf20f4d3082224874e05
tariba <tariqxa@gmail.com>
2023-04-25 09:59:28 +0100

    filtering task done

--- 036_filtering.py
+++ 036_filtering.py
@@ -37,7 +37,11 @@ print("Function: only_positive_numbers")
 
 # Return a new list with only the positive numbers
 def only_positive_numbers(numbers):
-  pass
+  positive_numbers=[]
+  for item in numbers:
+    if item > 0:
+      positive_numbers.append(item)
+  return positive_numbers
 
 check_that_these_are_equal(
   only_positive_numbers([-4, 4, -3, 3]), [4, 3])
